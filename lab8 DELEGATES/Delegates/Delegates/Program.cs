1b0a5f51685c2cf2d09f03ee953fca72cdf4aa21
dorenyerbolov <doren.yerbolov@gmail.com>
2018-04-03 03:02:00 +0600

    delegates

---
+++ lab8 DELEGATES/Delegates/Delegates/Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace Delegates
+{
+    class Program
+    {
+        delegate void myDelegate(int x, int b);
+        static void Main(string[] args)
+        {
+            myDelegate sumOfNumbers;
+            Sum sum = new Sum();
+            string s = Console.ReadLine();
+            string[] nums = s.Split(' ');
+            int x = int.Parse(nums[0]);
+            int y = int.Parse(nums[1]);
+
+            while (true)
+            {
+                Thread.Sleep(10000);
+                sumOfNumbers = sum.SumOfNumbers;
+                sumOfNumbers.Invoke(x, y);
+            }
+
+
+        }
+
+    }
+}
---
+++ lab8 DELEGATES/Delegates/Delegates/Sum.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Delegates
+{
+    class Sum
+    {
+        public void SumOfNumbers(int x, int y)
+        {
+            Console.WriteLine(x + y);
+        }
+    }
+    
+}
